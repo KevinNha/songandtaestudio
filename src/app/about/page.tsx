cd59deb1ad6113777c7e2b1274bcdcece03f46b0
ifandhope <gitechnhan@gmail.com>
2024-11-01 17:11:49 -0700

    Props updated—component refreshed accordingly!

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,47 +1,5 @@
-// import React from "react";
-// import Review from "@/components/Review";
-
-// const reviews = [
-//   {
-//     name: "Olivia",
-//     review:
-//       "Having to see my old dog's portrait made me feel that he was alive again. Such memories can be brought back to life with great sincerity. Thank you.",
-//     backgroundColor: "#FED7AA",
-//   },
-//   {
-//     name: "Peter",
-//     review:
-//       "Such art should never be forgotten. I love what I've received back. The colors are so vibrant that they feel alive. The time and the effort that was spent for my children will never be forgotten.",
-//     backgroundColor: "#FED7AA",
-//   },
-//   {
-//     name: "Claire",
-//     review:
-//       "I'm now debt-free and saving for a home because of your help. Thank you. Dagraw!",
-//     backgroundColor: "#FED7AA",
-//   },
-// ];
-
-// const Page = () => {
-//   return (
-//     <>
-//       <div>about placeholder</div>
-//       <Review reviews={reviews} />
-//     </>
-//   );
-// };
-
-// export default Page;
-
 import React from 'react';
 import Review from '@/components/Review';
-
-// const review = {
-//   name: 'Olivia',
-//   review:
-//     "Having to see my old dog's portrait made me feel that he was alive again. Such memories can be brought back to life with great sincerity. Thank you.",
-//   backgroundColor: '#FED7AA',
-// };
 
 const reviews = [
   {
@@ -63,24 +21,11 @@ const reviews = [
     backgroundColor: '#FED7AA',
   },
 ];
-//   {
-//     name: "Peter",
-//     review:
-//       "Such art should never be forgotten. I love what I've received back. The colors are so vibrant that they feel alive. The time and the effort that was spent for my children will never be forgotten.",
-//     backgroundColor: "#FED7AA",
-//   },
-//   {
-//     name: "Claire",
-//     review:
-//       "I'm now debt-free and saving for a home because of your help. Thank you. Dagraw!",
-//     backgroundColor: "#FED7AA",
-//   },
-// ];
 
 const Page = () => {
   return (
     <>
-      <h1 className="font-bold text-xl text-gray-400 text-center">
+      <h1 className="font-bold text-xl text-gray-400 text-center mt-20 mb-10">
         Client Review
       </h1>
       {reviews.map((review: any) => {
--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,105 +1,3 @@
-// import React from "react";
-
-// type Review = {
-//   name: string;
-//   review: string;
-//   backgroundColor: string;
-// };
-
-// type ReviewProps = {
-//   reviews: Review[];
-// };
-
-// const Review = ({ reviews }: ReviewProps) => {
-//   return (
-//     <div className="w-full py-[5rem] px-4 bg-transparent">
-//       <h1 className="font-bold text-xl text-gray-400">Client Reviews</h1>
-//       <div className="max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8">
-//         {reviews.map((review, index) => (
-//           <div
-//             key={index}
-//             className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
-//             style={{ backgroundColor: review.backgroundColor }}
-//           >
-//             <h3 className="text-sm font-normal">{review.review}</h3>
-//             <p className="italic text-left text-xs font-thin mt-auto pt-3">
-//               - {review.name}
-//             </p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Review;
-
-// import React from "react";
-
-// type Review = {
-//   name: string;
-//   review: string;
-//   backgroundColor: string;
-// };
-
-// type ReviewProps = {
-//   review: Review;
-// };
-
-// const Review = ({ review }: ReviewProps) => {
-//   return (
-//     <div className="w-full py-[5rem] px-4 bg-transparent">
-//       <h1 className="font-bold text-xl text-gray-400">Client Reviews</h1>
-//       <div className="max-w-[1240px] mx-auto grid grid-cols-1 md:grid-col-2 gap-8">
-//         <div
-//           className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
-//           style={{ backgroundColor: review.backgroundColor }}
-//         >
-//           <h3 className="text-sm font-normal">{review.review}</h3>
-//           <p className="italic text-left text-xs font-thin mt-auto pt-3">
-//             - {review.name}
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Review;
-
-// import React from "react";
-
-// type Review = {
-//   name: string;
-//   review: string;
-//   backgroundColor: string;
-// };
-
-// type ReviewProps = {
-//   review: Review;
-// };
-
-// const Review = ({ review }: ReviewProps) => {
-//   return (
-//     <div className="w-full py-[5rem] px-4 bg-transparent">
-//       <h1 className="font-bold text-xl text-gray-400">Client Reviews</h1>
-//       <div className="max-w-[1240px] mx-auto grid grid-cols-1 md:grid-col-2 gap-8">
-//         <div
-//           className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
-//           style={{ backgroundColor: review.backgroundColor }}
-//         >
-//           <h3 className="text-sm font-normal">{review.review}</h3>
-//           <p className="italic text-left text-xs font-thin mt-auto pt-3">
-//             - {review.name}
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Review;
-
 import React from 'react';
 
 type ReviewProps = {
@@ -114,7 +12,7 @@ const Review = (reviewProps: ReviewProps) => {
   const backgroundColor = reviewProps.backgroundColor;
 
   return (
-    <div className="w-full py-[5rem] px-4 bg-transparent flex justify-center">
+    <div className="w-full py-2 px-4 bg-transparent flex justify-center">
       <div className="w-full md:w-1/2">
         <div
           className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
