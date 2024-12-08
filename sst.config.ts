0f8fa0307ef5510ae64cc6906a1a6eaecfa3e632
Hyunjin Nha <hyunjinnha@gmail.com>
2024-12-08 00:11:51 -0800

    Change autodeploy settings to freetier

--- sst.config.ts
+++ sst.config.ts
@@ -131,4 +131,15 @@ export default $config({
       ],
     });
   },
+
+  console: {
+    autodeploy: {
+      runner: {
+        engine: 'codebuild',
+        architecture: 'x86_64',
+        compute: 'small',
+        timeout: '1 hour',
+      },
+    },
+  },
 });
