1da310e258dcde270281794269fb0fab2466abd8
Hyunjin Nha <hyunjinnha@gmail.com>
2025-04-13 16:38:56 -0700

    Improve responsive design + fonts

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -8,9 +8,10 @@ import {
   useRef,
 } from 'react';
 import { sendEmail } from '@/app/contact/actions';
-import { Lato } from 'next/font/google';
+import { Lato, Raleway } from 'next/font/google';
 
 const lato = Lato({ weight: '400', subsets: ['latin'] });
+const raleway = Raleway({ weight: ['400', '600'], subsets: ['latin'] });
 
 export default function ContactForm() {
   const [submitted, setSubmitted] = useState(false);
@@ -26,6 +27,7 @@ export default function ContactForm() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setSubmitted(true);
 
     const formData = new FormData(e.currentTarget);
     startTransition(() => {
@@ -35,13 +37,15 @@ export default function ContactForm() {
 
   if (submitted) {
     return (
-      <div
-        className={`flex flex-col p-10 border-2 rounded-lg gap-4 ${lato.className}`}
-      >
-        <h1 className="text-2xl font-bold">Thank you for your message!</h1>
-        <p>We will get back to you as soon as possible.</p>
+      <div className="flex flex-col p-10 border-2 rounded-lg gap-4">
+        <h1 className={`${raleway.className} text-2xl font-bold`}>
+          Thank you for your message!
+        </h1>
+        <p className={`${lato.className}`}>
+          We will get back to you as soon as possible.
+        </p>
         <button
-          className="bg-[#111111] text-white p-2 rounded-lg mt-4 hover:bg-[#333333] transition-colors duration-300"
+          className={`bg-[#111111] text-white p-2 rounded-lg mt-4 hover:bg-[#333333] transition-colors duration-300 ${lato.className}`}
           onClick={() => {
             setSubmitted(false);
           }}
@@ -53,21 +57,22 @@ export default function ContactForm() {
   }
 
   return (
-    <div
-      className={`flex flex-col p-10 border-2 rounded-lg gap-10 ${lato.className}`}
-    >
-      <h1 className="text-2xl font-bold">Contact Us</h1>
+    <div className="flex flex-col p-10 border-2 rounded-lg gap-10">
+      <h1 className={`${raleway.className} text-2xl font-bold`}>Contact Us</h1>
       <form
         className="flex flex-col gap-4"
         onSubmit={handleSubmit}
         ref={formRef}
       >
         <div className="flex flex-col gap-2">
-          <label className="font-bold text-sm" htmlFor="name">
+          <label
+            className={`${raleway.className} font-bold text-sm`}
+            htmlFor="name"
+          >
             Name <span className="text-red-500">*</span>
           </label>
           <input
-            className="border-2 rounded-lg p-2 appearance-none focus:outline-none focus:ring-2 focus:ring-gray-500 bg-transparent"
+            className={`border-2 rounded-lg p-2 appearance-none focus:outline-none focus:ring-2 focus:ring-gray-500 bg-transparent ${lato.className}`}
             type="text"
             name="name"
             placeholder="Your name"
@@ -75,11 +80,14 @@ export default function ContactForm() {
           <p className="text-red-500">{state?.errors?.name?.[0]}</p>
         </div>
         <div className="flex flex-col gap-2">
-          <label className="font-bold text-sm" htmlFor="email">
+          <label
+            className={`${raleway.className} font-bold text-sm`}
+            htmlFor="email"
+          >
             Email <span className="text-red-500">*</span>
           </label>
           <input
-            className="border-2 rounded-lg p-2 appearance-none focus:outline-none focus:ring-2 focus:ring-gray-500 bg-transparent"
+            className={`border-2 rounded-lg p-2 appearance-none focus:outline-none focus:ring-2 focus:ring-gray-500 bg-transparent ${lato.className}`}
             type="text"
             name="email"
             placeholder="your.email@example.com"
@@ -87,11 +95,14 @@ export default function ContactForm() {
           <p className="text-red-500">{state?.errors?.email?.[0]}</p>
         </div>
         <div className="flex flex-col gap-2">
-          <label className="font-bold text-sm" htmlFor="message">
+          <label
+            className={`${raleway.className} font-bold text-sm`}
+            htmlFor="message"
+          >
             Message <span className="text-red-500">*</span>
           </label>
           <textarea
-            className="border-2 rounded-lg p-2 appearance-none focus:outline-none focus:ring-2 focus:ring-gray-500 bg-transparent"
+            className={`border-2 rounded-lg p-2 appearance-none focus:outline-none focus:ring-2 focus:ring-gray-500 bg-transparent ${lato.className}`}
             placeholder="For inquiries, please contact us regarding gallery or art show invitations, portrait requests, or collaboration opportunities"
             rows={5}
             name="message"
@@ -99,7 +110,7 @@ export default function ContactForm() {
           <p className="text-red-500">{state?.errors?.message?.[0]}</p>
         </div>
         <button
-          className="bg-[#111111] text-white p-2 rounded-lg hover:bg-[#333333] transition-colors duration-300"
+          className={`bg-[#111111] text-white p-2 rounded-lg hover:bg-[#333333] transition-colors duration-300 ${lato.className}`}
           type="submit"
           disabled={pending}
         >
